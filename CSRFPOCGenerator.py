190a5e4984dadc77d266694e133cf7d01a082843
jayeshchauhan <jayesh.sngh@gmail.com>
2013-01-23 19:17:02 +0530

    Update CSRFPOCGenerator.py

--- CSRFPOCGenerator.py
+++ CSRFPOCGenerator.py
@@ -1,3 +1,22 @@
+#
+# Copyright 2013 Jayesh Singh Chauhan
+#
+# This file is part of CSRFPOCGenerator
+#
+# CSRFPOCGenerator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# CSRFPOCGenerator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with CSRFPOCGenerator.  If not, see http://www.gnu.org/licenses/.
+#
+
+
 from IronWASP import *
 
 class CSRFPOCGenerator(Module):
